f1fc0312f56fcba33428210a273aa4c809f3c6b4
cdorsman <39407105+cdorsman@users.noreply.github.com>
2018-05-18 21:30:16 +0200

    Update template.py
    Cleaning up the code
    corrected exception handling of config parsing

--- template.py
+++ template.py
@@ -14,7 +14,14 @@ import logging
 import logging.config
 import logging.handlers
 
-PROGNAME = os.path.basename(__file__)
+# The PROGNAME global is how you want to name your script. 
+# Once PROGNAME is set, the script will look for configfile called PROGNAME.conf
+# If you want to name the program as the filename but without the '.py' extension.
+PROGNAME = os.path.basename(__file__).rstrip('.py')
+# If you want to set a custom name:
+#PROGNAME = "progname"
+
+LOGGERNAME = PROGNAME.title()
 VERSION = "0.1"
 
 class Options(object):
@@ -28,7 +35,6 @@ class Options(object):
 
     @classmethod
     def __init__(self, args):
-        self.verbose = False
         """ 
         Parsing cli options
     
@@ -36,10 +42,9 @@ class Options(object):
             - '-C' or '--conf='         : Location of configuration file.
             - '--help'                  : Print usage 
             - '--version'               : Print version 
-
-        Mandatory: env and datasource(s) or workbook(s)
         """ 
         self.configfile = PROGNAME + ".conf"
+        self.logconffile = "./logging.ini"
 
         try:
             opts, args = getopt.getopt(args, ':C:vV',
@@ -54,15 +59,9 @@ class Options(object):
             elif opt == '--help':
                 self.Usage()
                 sys.exit(0)
-            elif opt in ('-v', '--verbose'):
-                self.verbose = True
-            elif opt in ('-V', '--version'):
+            elif opt ==  '--version':
                 self.Version()
                 sys.exit(0)
-
-        if not self.env:
-            self.Usage()
-            sys.exit(1)
 
     @classmethod
     def Version(self):
@@ -87,19 +86,20 @@ class Config(object):
     @classmethod
     def __init__(self):
         self.cfg = ConfigParser.RawConfigParser()
-        self.env = op.env
-        self.proj_customer = ""
 
         try:
+            log.info('Reading configuration file')
             if op.configfile and os.R_OK:
                 self.Read(op.configfile)
         except IOError as io_err:
             log.error("Configuration file cannot be read: %s", io_err)
             sys.exit(1)
         
-        # Customer definition
-        self.main_arg = self.Getoption('main', 'argument', None, '')       
-        self.env_arg = self.Getoption(self.env, 'dbuser', None, '')
+        # Example option for main with default value which isn't required 
+        #self.main_first_arg = self.Getoption('main', 'option1', None, '', False)       
+        
+        # Example option for main with default value which is required 
+        self.main_sec_opt = self.Getoption('main', 'option2', None, 'argument1', True)       
 
     @classmethod
     def Read(self, configfile):
@@ -113,52 +113,61 @@ class Config(object):
         else:
             log.debug("Cannot open file %s", configfile)
             sys.exit(1)
-
+    
     @classmethod
-    def Getoption(self, section, option, var, default):
-        """
-        Get all options from defined sections 
+    def Getoption(self, section, option, var, default, required):
+        """ 
+        Get from defined sections all the defined options 
         if option is not defined set default value. 
-        """
+        """ 
         try:
-            var = self.cfg.get(section, option)
-            # If option is not set then the default setting will be replace the empty variable
-            if not var:
-                var = self.cfg.set(section, option, default)
-                log.debug('Option %s in section [%s] to: %s (default)', option, section, default)
-            else:
-                log.debug('Setting option %s in section [%s] to: %s', option, section, var)
+            try:
+                var = self.cfg.get(section, option)
+                # If option is not set then the default setting will be replace the empty variable
+                if not var and not default and not required:
+                    log.warn('Skipping non required option [%s] in section [%s] defined without default', 
+                                option, section)
+                elif not var and default and required: 
+                    var = self.cfg.set(section, option, default)
+                    log.debug('Option [%s] in section [%s] to: %s (default)', option, section, default)
+                elif not var and not default and required: 
+                    log.error('Required option [%s] in section [%s] is undefined and missing defined default',
+                                option, section)
+                    sys.exit(1)
+                else:
+                    log.debug('Setting option [%s] in section [%s] to: %s', option, section, var)
+            except ConfigParser.NoOptionError, opt_err:
+                log.error('Failed to find option [%s]', option)
+                raise LookupError(opt_err)
         except ConfigParser.NoSectionError, sect_err:
             log.error('Failed to find section [%s]', section)
             raise LookupError(sect_err)
-        return var
+        return var 
     
 def main():
     """
-    After parsing CLI arguments the following steps will be done
-    - Configuration file progname.conf will be parsed and arguments will be loaded
+    After CLI arguments the following steps will be done
+    - Configfile PROGNAME.conf will be parsed.
     """
     
-    log.info('Reading configuration file')
     cf = Config()
+    print "Main"
 
 if __name__ == '__main__':
     """
-    - The commandline arguments are passed to the Options class
-    - Initialization of the logger
-    - Printing first line with the following:
+    - CLI arguments are parsed in Options class
+    - Printing first line with script info with the following:
         - Progname 
         - Version 
         - Hardware platform 
         - Python version 
-        
-    Example of print: progname.py-0.1 running on Python version: 2.7.10-x86_64 on Linux
     """
+
     op = Options(sys.argv[1:])
     # Location of logging.ini
-    logging.config.fileConfig("logging.ini")
-    log = logging.getLogger(PROGNAME)
-    log.debug('Initializing logger')
-    log.info('%s Version %s running on Python version: %s-%s on %s',
-              PROGNAME, VERSION, platform.python_version(), platform.machine(), platform.system())
+    logging.config.fileConfig(op.logconffile)
+    log = logging.getLogger(LOGGERNAME)
+    log.debug('%s-%s running on Python version: %s-%s on %s',
+              LOGGERNAME, VERSION, platform.python_version(), 
+              platform.machine(), platform.system())
     main()
